8d8aaca59748550d55476661e11513b566d8a691
chrisfromtemporaryid@gmail.com <chrisfromtemporaryid@gmail.com>
2017-08-14 17:52:35 -0400

    Start initial work of create workout view, fix some issues with buttons

--- Lifting Buddy/CreateWorkoutView.swift
+++ Lifting Buddy/CreateWorkoutView.swift
@@ -8,15 +8,87 @@
 
 import UIKit
 
-class CreateWorkoutView: UIView {
+class CreateWorkoutView: UIScrollView {
+    
+    // View properties
+    
+    var nameEntryField: UITextField?
+    
+    let viewPadding: CGFloat = 20.0
     
     // View overrides
     
     override func layoutSubviews() {
-        let testView: ExerciseButton = ExerciseButton(frame: CGRect(x: 10,
-                                                                    y: 10,
-                                                                    width: self.frame.width - 20,
-                                                                    height: 50), exercise: Exercise())
-        self.addSubview(testView)
+        self.backgroundColor = UIColor.niceGray()
+        
+        // Disable horizontal scrolling
+        self.setContentOffset(CGPoint(x: 0, y: self.contentOffset.y), animated: false)
+        self.isDirectionalLockEnabled = true
+        
+        // Current height we append to
+        var curHeight: CGFloat = 20
+        
+        // NAME VIEW
+        
+        // Containing view for the name entry label and view
+        let nameEntryContainingView: UIView = UIView(frame: CGRect(x: 10,
+                                                         y: curHeight,
+                                                         width: self.frame.width - 20,
+                                                         height: 70))
+        
+        let nameLabel: UILabel = UILabel(frame: CGRect(x: 0,
+                                                       y: 0,
+                                                       width: nameEntryContainingView.frame.width,
+                                                       height: 20))
+        nameLabel.text = "Name of New Workout"
+        nameLabel.font = UIFont.boldSystemFont(ofSize: 18.0)
+        nameLabel.textAlignment = .center
+        nameLabel.textColor = UIColor.niceBlue()
+        curHeight += 10
+        nameEntryContainingView.addSubview(nameLabel)
+        
+        // If we can fit the enclosed view width in the view, add it.
+        if nameEntryContainingView.frame.width >= 250 {
+            // Display in a middle of view with max width of 50
+            nameEntryField = UITextField(frame: CGRect(x: (nameEntryContainingView.frame.width - 250) / 2,
+                                                       y: 30,
+                                                       width: 250,
+                                                       height: 40))
+        } else { // Otherwise, fit a view that fits.
+            nameEntryField = UITextField(frame: CGRect(x: 0,
+                                                       y: 30,
+                                                       width: nameEntryContainingView.frame.width,
+                                                       height: 20))
+        }
+        
+        // View select / deselect events
+        nameEntryField?.addTarget(self, action: #selector(textfieldSelected(sender:)), for: .editingDidBegin)
+        nameEntryField?.addTarget(self, action: #selector(textfieldDeselected(sender:)), for: .editingDidEnd)
+        
+        // View prettiness
+        nameEntryField?.layer.cornerRadius = 5.0
+        nameEntryField?.textAlignment = .center
+        nameEntryField?.placeholder = "Name of New Workout"
+        textfieldDeselected(sender: nameEntryField!)
+        
+        nameEntryContainingView.addSubview(nameEntryField!)
+        
+        // Add height of the containing view for name entry + additional padding
+        // to prevent overlap in views
+        curHeight += nameEntryContainingView.frame.height + viewPadding
+        
+        self.addSubview(nameEntryContainingView)
+    }
+    
+    // Event functions
+    
+    @objc func textfieldSelected(sender: UITextField) {
+        sender.backgroundColor = UIColor.niceYellow()
+        sender.textColor = UIColor.white
+    }
+    
+    @objc func textfieldDeselected(sender: UITextField) {
+        sender.backgroundColor = UIColor.white
+        sender.textColor = UIColor.black
     }
 }
--- Lifting Buddy/WorkoutsView.swift
+++ Lifting Buddy/WorkoutsView.swift
@@ -22,24 +22,24 @@ class WorkoutsView: UIView {
     // MARK: View Overrides
     
     override func layoutSubviews() {
-        if floatyButton != nil {
-            floatyButton?.removeFromSuperview()
+        if floatyButton == nil {
+            floatyButton = PrettyButton(frame: CGRect(x: self.frame.maxX - 100,
+                                                      y: self.frame.maxY - 100,
+                                                      width: 75,
+                                                      height: 75))
+            floatyButton?.backgroundColor = UIColor.niceBlue()
+            floatyButton?.setOverlayStyle(style: .BLOOM)
+            floatyButton?.cornerRadius = floatyButton!.frame.width / 2
+            floatyButton?.shadowOpacity = 0.2
+            floatyButton?.setOverlayColor(color: UIColor.niceYellow())
+            floatyButton?.setTitle("Create", for: .normal)
+            floatyButton?.layer.zPosition = 90
+            floatyButton?.addTarget(self, action: #selector(showCreateWorkoutView(sender:)), for: .touchUpInside)
+            
+            self.addSubview(floatyButton!)
+        } else {
+            floatyButton?.layoutSubviews()
         }
-        
-        floatyButton = PrettyButton(frame: CGRect(x: self.frame.maxX - 100,
-                                                                    y: self.frame.maxY - 100,
-                                                                    width: 75,
-                                                                    height: 75))
-        floatyButton?.backgroundColor = UIColor.niceBlue()
-        floatyButton?.setOverlayStyle(style: .BLOOM)
-        floatyButton?.cornerRadius = floatyButton!.frame.width / 2
-        floatyButton?.shadowOpacity = 0.2
-        floatyButton?.setOverlayColor(color: UIColor.niceYellow())
-        floatyButton?.setTitle("Create", for: .normal)
-        floatyButton?.layer.zPosition = 1
-        floatyButton?.addTarget(self, action: #selector(showCreateWorkoutView(sender:)), for: .touchUpInside)
-        
-        self.addSubview(floatyButton!)
         
         super.layoutSubviews()
     }
@@ -47,12 +47,30 @@ class WorkoutsView: UIView {
     // MARK: Event functions
     
     @objc func showCreateWorkoutView(sender: PrettyButton) {
+        
+        if floatyButton != nil {
+            floatyButton?.isEnabled = false
+        }
+        
         let createWorkoutView: CreateWorkoutView =
             CreateWorkoutView(frame: CGRect(x: 0,
-                                            y: self.frame.minY,
+                                            y: -self.frame.height,
                                             width: self.frame.width,
                                             height: self.frame.height))
+        createWorkoutView.layer.zPosition = 100
         self.addSubview(createWorkoutView)
+        
+        UIView.animate(withDuration: 0.5, animations: {
+            createWorkoutView.frame = CGRect(x: 0,
+                                             y: self.frame.minY,
+                                             width: self.frame.width,
+                                             height: self.frame.height)
+            
+            self.floatyButton?.frame = CGRect(x: (self.floatyButton?.frame.minX)!,
+                                              y: self.frame.maxY + 200,
+                                              width: 50,
+                                              height: 50)
+        })
     }
     
     func showExercises() {

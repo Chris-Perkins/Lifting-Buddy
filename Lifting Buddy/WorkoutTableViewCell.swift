ef889f2a079569342761926a45f61ea272cf0923
chrisfromtemporaryid@gmail.com <chrisfromtemporaryid@gmail.com>
2017-09-04 17:50:06 -0400

    create workout tableview base

---
+++ Lifting Buddy/WorkoutTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  WorkoutTableViewCell.swift
+//  Lifting Buddy
+//
+//  Created by Christopher Perkins on 9/4/17.
+//  Copyright © 2017 Christopher Perkins. All rights reserved.
+//
+
+import Foundation

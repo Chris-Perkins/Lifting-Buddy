d1427d5f60fc66942394bc9e2d15f99befa9959b
chrisfromtemporaryid@gmail.com <chrisfromtemporaryid@gmail.com>
2017-09-09 12:28:23 -0400

    Create WorkoutStartView

---
+++ Lifting Buddy/WorkoutStartView.swift
@@ -0,0 +1,9 @@
+//
+//  WorkoutStartView.swift
+//  Lifting Buddy
+//
+//  Created by Christopher Perkins on 9/9/17.
+//  Copyright © 2017 Christopher Perkins. All rights reserved.
+//
+
+import Foundation

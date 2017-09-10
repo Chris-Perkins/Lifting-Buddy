33d418db43e33228832968441c8bb89014eba499
chrisfromtemporaryid@gmail.com <chrisfromtemporaryid@gmail.com>
2017-09-10 14:05:27 -0400

    Convert sectionview into constraints

--- Lifting Buddy/SectionView.swift
+++ Lifting Buddy/SectionView.swift
@@ -16,12 +16,12 @@ class SectionView: UIView {
     // Required view for modifying sectionContentView
     var mainViewController: MainViewController?
     // Our sections
-    var homeButton: PrettyButton?
-    var workoutsButton: PrettyButton?
-    var exercisesButton: PrettyButton?
-    var settingsButton: PrettyButton?
+    private var homeButton: PrettyButton
+    private var workoutsButton: PrettyButton
+    private var exercisesButton: PrettyButton
+    private var settingsButton: PrettyButton
     
-    var selectedView: PrettyButton?
+    private var selectedView: PrettyButton?
     
     
     // MARK: Enums
@@ -37,7 +37,22 @@ class SectionView: UIView {
     // MARK: Init functions
     
     override init(frame: CGRect) {
+        homeButton = PrettyButton()
+        workoutsButton = PrettyButton()
+        exercisesButton = PrettyButton()
+        settingsButton = PrettyButton()
+        
         super.init(frame: frame)
+        
+        self.addSubview(homeButton)
+        self.addSubview(workoutsButton)
+        self.addSubview(exercisesButton)
+        self.addSubview(settingsButton)
+        
+        self.createAndActivateHomeButtonConstraints()
+        self.createAndActivateWorkoutsButtonConstraints()
+        self.createAndActivateExercisesButtonConstraints()
+        self.createAndActivateSettingsButtonConstraints()
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -51,55 +66,25 @@ class SectionView: UIView {
         
         self.mainViewController = (self.next?.next?.next as! MainViewController)
         
-        let width: CGFloat = self.frame.width / 4
-        
-        // Today Button
-        homeButton =
-            PrettyButton(frame: CGRect(x: 0,
-                                       y: 0,
-                                    width: width,
-                                    height: self.frame.height))
-        
-        homeButton?.setTitle("home", for: .normal)
-        setButtonProperties(button: homeButton!)
-        self.addSubview(homeButton!)
+        // Home Button
+        homeButton.setTitle("home", for: .normal)
+        setButtonProperties(button: homeButton)
         
         // Workouts Button
-        workoutsButton =
-            PrettyButton(frame: CGRect(x: width,
-                                       y: 0,
-                                       width: width,
-                                       height: self.frame.height))
-        
-        workoutsButton?.setTitle("workouts", for: .normal)
-        setButtonProperties(button: workoutsButton!)
-        self.addSubview(workoutsButton!)
+        workoutsButton.setTitle("workouts", for: .normal)
+        setButtonProperties(button: workoutsButton)
         
         
-        // Statistics Button
-        exercisesButton =
-            PrettyButton(frame: CGRect(x: width * 2,
-                                       y: 0,
-                                       width: width,
-                                       height: self.frame.height))
-        
-        exercisesButton?.setTitle("exercises", for: .normal)
-        setButtonProperties(button: exercisesButton!)
-        self.addSubview(exercisesButton!)
+        // Exercises Button
+        exercisesButton.setTitle("exercises", for: .normal)
+        setButtonProperties(button: exercisesButton)
         
         // Settings Button
-        settingsButton =
-            PrettyButton(frame: CGRect(x: width * 3,
-                                       y: 0,
-                                       width: width,
-                                       height: self.frame.height))
-        
-        settingsButton?.setTitle("settings", for: .normal)
-        setButtonProperties(button: settingsButton!)
-        self.addSubview(settingsButton!)
+        settingsButton.setTitle("settings", for: .normal)
+        setButtonProperties(button: settingsButton)
         
         // Start on the today button
-        buttonPress(sender: homeButton!)
+        buttonPress(sender: homeButton)
     }
     
     // MARK: Private functions
@@ -123,16 +108,16 @@ class SectionView: UIView {
             var viewType: SectionView.ContentViews? = nil
             
             switch(sender) {
-            case (self.homeButton!):
+            case (self.homeButton):
                 viewType = .HOME
                 break
-            case (self.workoutsButton!):
+            case (self.workoutsButton):
                 viewType = .WORKOUTS
                 break
-            case (self.exercisesButton!):
+            case (self.exercisesButton):
                 viewType = .EXERCISES
                 break
-            case (self.settingsButton!):
+            case (self.settingsButton):
                 viewType = .SETTINGS
                 break
             default:
@@ -145,5 +130,145 @@ class SectionView: UIView {
         
         // Set the newly selected view equal to this button
         selectedView = sender
+    }
+    
+    // Mark: Constraint functions
+    
+    // Cling to top, left ; height of this view ; width of this view / 4
+    func createAndActivateHomeButtonConstraints() {
+        homeButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint(item: self,
+                           attribute: .top,
+                           relatedBy: .equal,
+                           toItem: homeButton,
+                           attribute: .top,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        NSLayoutConstraint(item: self,
+                           attribute: .left,
+                           relatedBy: .equal,
+                           toItem: homeButton,
+                           attribute: .left,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        NSLayoutConstraint(item: self,
+                           attribute: .height,
+                           relatedBy: .equal,
+                           toItem: homeButton,
+                           attribute: .height,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        NSLayoutConstraint(item: self,
+                           attribute: .width,
+                           relatedBy: .equal,
+                           toItem: homeButton,
+                           attribute: .width,
+                           multiplier: 4,
+                           constant: 0).isActive = true
+    }
+    
+    // cling to top of view, right of homeButton ; height of this homebutton ; width of this view / 4
+    func createAndActivateWorkoutsButtonConstraints() {
+        workoutsButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint(item: homeButton,
+                           attribute: .top,
+                           relatedBy: .equal,
+                           toItem: workoutsButton,
+                           attribute: .top,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        NSLayoutConstraint(item: homeButton,
+                           attribute: .right,
+                           relatedBy: .equal,
+                           toItem: workoutsButton,
+                           attribute: .left,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        NSLayoutConstraint(item: homeButton,
+                           attribute: .height,
+                           relatedBy: .equal,
+                           toItem: workoutsButton,
+                           attribute: .height,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        NSLayoutConstraint(item: self,
+                           attribute: .width,
+                           relatedBy: .equal,
+                           toItem: workoutsButton,
+                           attribute: .width,
+                           multiplier: 4,
+                           constant: 0).isActive = true
+    }
+    
+    // cling to top of homeButton, right of workoutsButton ; height of exercisesButton ;
+    // width of this view / 4
+    func createAndActivateExercisesButtonConstraints() {
+        exercisesButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint(item: homeButton,
+                           attribute: .top,
+                           relatedBy: .equal,
+                           toItem: exercisesButton,
+                           attribute: .top,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        NSLayoutConstraint(item: workoutsButton,
+                           attribute: .right,
+                           relatedBy: .equal,
+                           toItem: exercisesButton,
+                           attribute: .left,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        NSLayoutConstraint(item: homeButton,
+                           attribute: .height,
+                           relatedBy: .equal,
+                           toItem: exercisesButton,
+                           attribute: .height,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        NSLayoutConstraint(item: self,
+                           attribute: .width,
+                           relatedBy: .equal,
+                           toItem: exercisesButton,
+                           attribute: .width,
+                           multiplier: 4,
+                           constant: 0).isActive = true
+    }
+    
+    // cling to top of homeButton, right of exercisesButton ; height of exercisesButton ;
+    // width of this view / 4
+    func createAndActivateSettingsButtonConstraints() {
+        settingsButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint(item: homeButton,
+                           attribute: .top,
+                           relatedBy: .equal,
+                           toItem: settingsButton,
+                           attribute: .top,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        NSLayoutConstraint(item: exercisesButton,
+                           attribute: .right,
+                           relatedBy: .equal,
+                           toItem: settingsButton,
+                           attribute: .left,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        NSLayoutConstraint(item: homeButton,
+                           attribute: .height,
+                           relatedBy: .equal,
+                           toItem: settingsButton,
+                           attribute: .height,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        NSLayoutConstraint(item: self,
+                           attribute: .width,
+                           relatedBy: .equal,
+                           toItem: settingsButton,
+                           attribute: .width,
+                           multiplier: 4,
+                           constant: 0).isActive = true
     }
 }

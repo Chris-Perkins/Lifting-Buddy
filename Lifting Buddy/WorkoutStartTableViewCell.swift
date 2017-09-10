78aad1da9200c0072295924be90b3294f307385f
chrisfromtemporaryid@gmail.com <chrisfromtemporaryid@gmail.com>
2017-09-10 19:04:52 -0400

    Create WorkoutStartTableView + Cell

---
+++ Lifting Buddy/WorkoutStartTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  WorkoutStartTableViewCell.swift
+//  Lifting Buddy
+//
+//  Created by Christopher Perkins on 9/10/17.
+//  Copyright © 2017 Christopher Perkins. All rights reserved.
+//
+
+import Foundation

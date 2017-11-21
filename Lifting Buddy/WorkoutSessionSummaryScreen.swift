88b525a5ef2c72b738550c4b9cfb2597c8d02fba
chrisfromtemporaryid@gmail.com <chrisfromtemporaryid@gmail.com>
2017-11-20 20:48:25 -0500

    Can now close that stupid menu
    The stupid menu being blank is temporary. I promise.

--- Lifting Buddy/WorkoutSessionSummaryScreen.swift
+++ Lifting Buddy/WorkoutSessionSummaryScreen.swift
@@ -14,30 +14,55 @@ class WorkoutSessionSummaryScreen: UIView {
     
     // MARK: View properties
     
+    // A button the user should press if we're trying to close this view
     let closeButton: PrettyButton
     
     // MARK: Init methods
     
     init(withExercises: List<Exercise>) {
-        closeButton = PrettyButton()
+        self.closeButton = PrettyButton()
         
         super.init(frame: .zero)
         
-        self.backgroundColor = UIColor.niceBlue
+        self.addSubview(closeButton)
+        
+        self.closeButton.addTarget(self,
+                                   action: #selector(buttonPress(sender:)),
+                                   for: .touchUpInside)
+        
+        self.createAndActivateCloseButtonConstraints()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: View overrides
+    
     override func layoutSubviews() {
         super.layoutSubviews()
+        
+        // This view's layout
+        self.backgroundColor = UIColor.niceGray
         
         // Close button
         self.closeButton.setDefaultProperties()
         self.closeButton.setTitle("Return to Main View",
                                   for: .normal)
     }
+    
+    // MARK: Event functions
+    
+    @objc private func buttonPress(sender: UIButton) {
+        switch sender {
+        case self.closeButton:
+            self.removeSelfNicelyWithAnimation()
+        default:
+            fatalError("Button pressed that was not set up")
+        }
+    }
+    
+    // MARK: Constraint Functions
     
     private func createAndActivateCloseButtonConstraints() {
         self.closeButton.translatesAutoresizingMaskIntoConstraints = false
@@ -48,6 +73,11 @@ class WorkoutSessionSummaryScreen: UIView {
         NSLayoutConstraint.createViewAttributeCopyConstraint(view: self.closeButton,
                                                              withCopyView: self,
                                                              attribute: .left).isActive = true
+        NSLayoutConstraint.createViewAttributeCopyConstraint(view: self.closeButton,
+                                                             withCopyView: self,
+                                                             attribute: .right).isActive = true
+        NSLayoutConstraint.createHeightConstraintForView(view: self.closeButton,
+                                                         height: 50).isActive = true
         
     }
 }

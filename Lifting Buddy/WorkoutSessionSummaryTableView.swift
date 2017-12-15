69673b5389af8dfc6192c23d27f115bdaa913eaa
chrisfromtemporaryid@gmail.com <chrisfromtemporaryid@gmail.com>
2017-12-14 20:43:55 -0500

    Summary screen no longer shows duplicate exercises

--- Lifting Buddy/WorkoutSessionSummaryTableView.swift
+++ Lifting Buddy/WorkoutSessionSummaryTableView.swift
@@ -14,14 +14,27 @@ class WorkoutSessionSummaryTableView: UITableView, UITableViewDataSource, UITabl
     
     // MARK: View properties
     
+    // The data for this view
     private let data: List<Exercise>
+    // However, we don't want to show multiple of the same exercise.
+    // Why? This would cause spaghetti-code if we wanted to show newData vs Old Data nicely.
+    // The average user probably shouldn't have the same exercise multiple times in a workout, anyway.
+    private var dataWithNoDuplicates: [Exercise]
     
     // MARK: View overrides
     
     init(workout: Workout?, exercises: List<Exercise>, style: UITableViewStyle) {
         data = exercises
+        dataWithNoDuplicates = [Exercise]()
         
         super.init(frame: .zero, style: style)
+        
+        // This could be done in O(n), but O(n^2) isn't even slow.
+        for exercise in exercises {
+            if !dataWithNoDuplicates.contains(exercise) {
+                dataWithNoDuplicates.append(exercise)
+            }
+        }
         
         setupTableView()
     }
@@ -34,13 +47,13 @@ class WorkoutSessionSummaryTableView: UITableView, UITableViewDataSource, UITabl
     
     // Data is what we use to fill in the table view
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data.count
+        return dataWithNoDuplicates.count
     }
     
     // Initialize our cells..
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! WorkoutSessionSummaryTableViewCell
-        cell.setExercise(exercise: data[indexPath.row],
+        cell.setExercise(exercise: dataWithNoDuplicates[indexPath.row],
                          withDateRecorded: WorkoutSessionView.dateLastInitialized)
         return cell
     }
